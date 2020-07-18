d5972a7bf1e2cdf2a42a61f96e52cccb54ab12f3
Eric Dahl <eric.c.dahl@gmail.com>
2020-07-18 14:14:03 -0700

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# tf_splunk_fargate_efs
+
+Demo app of Splunk running on AWS ECS Fargate with EFS for storage
+
+## TODO
+
+- Migrate docker build into different repo with GitHub Action?
+- push docker image to Docker Hub
--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ resource "aws_ecs_cluster" "cluster" {
 
 data "template_file" "splunk" {
   template = file("templates/splunk.json")
+
+  vars = {
+    efs_file_system_id = aws_efs_file_system.splunk.id
+  }
 }
 
 resource "aws_ecs_task_definition" "splunk" {
@@ -27,6 +31,15 @@ resource "aws_ecs_task_definition" "splunk" {
   network_mode = "awsvpc"
   cpu          = 4096
   memory       = 10240
+
+  volume {
+    name = "opt-splunk"
+
+    efs_volume_configuration {
+      file_system_id          = aws_efs_file_system.splunk.id
+      root_directory          = "/opt/splunk"
+    }
+  }
 }
 
 resource "aws_security_group" "splunk" {
@@ -64,6 +77,8 @@ resource "aws_ecs_service" "splunk" {
   desired_count   = 1
   launch_type     = "FARGATE"
 
+  platform_version = "1.4.0"
+
   network_configuration {
     security_groups = [
       aws_security_group.splunk.id,
@@ -71,10 +86,43 @@ resource "aws_ecs_service" "splunk" {
       module.vpc.sg_allow_vpc,
     ]
 
-    subnets = [module.vpc.subnet_public1]
+    subnets          = [module.vpc.subnet_public1]
     assign_public_ip = true
   }
 
   deployment_minimum_healthy_percent = 0
 
+}
+
+resource "aws_security_group" "splunk_efs" {
+  name   = "splunk-efs"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "splunk_efs_ingress_2049_splunk" {
+  security_group_id        = aws_security_group.splunk_efs.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 2049
+  to_port                  = 2049
+  source_security_group_id = aws_security_group.splunk.id
+  description              = "Allows Splunk SG to connec to EFS"
+}
+
+resource "aws_efs_file_system" "splunk" {
+
+  tags = {
+    Name = "tf_splunk_fargate_efs"
+  }
+}
+
+resource "aws_efs_mount_target" "splunk" {
+  file_system_id = aws_efs_file_system.splunk.id
+  subnet_id      = module.vpc.subnet_private1
+
+  security_groups = [
+    aws_security_group.splunk_efs.id,
+    module.vpc.sg_allow_vpc,  # not needed?
+    module.vpc.sg_allow_egress  # not needed?
+  ]
 }
