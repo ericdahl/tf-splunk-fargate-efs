50627b8763d51c6c21805caed41933fbbb74f98b
Eric Dahl <eric.c.dahl@gmail.com>
2020-07-18 21:12:42 -0700

    more attempts

--- main.tf
+++ main.tf
@@ -32,6 +32,8 @@ resource "aws_ecs_task_definition" "splunk" {
   cpu          = 4096
   memory       = 10240
 
+  execution_role_arn = aws_iam_role.splunk_execution_role.arn
+
   volume {
     name = "opt-splunk"
 
@@ -41,6 +43,7 @@ resource "aws_ecs_task_definition" "splunk" {
     }
   }
 }
+
 
 resource "aws_security_group" "splunk" {
   name   = "splunk"
@@ -125,4 +128,59 @@ resource "aws_efs_mount_target" "splunk" {
     module.vpc.sg_allow_vpc,  # not needed?
     module.vpc.sg_allow_egress  # not needed?
   ]
+}
+
+
+
+resource "aws_iam_role" "splunk_execution_role" {
+
+  name = "splunk-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_policy" "splunk_execution_role" {
+  name = "splunk-execution-role"
+  //  role = aws_iam_role.splunk_execution_role.id
+
+  policy =<<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "splunk_iam_execution" {
+  policy_arn = aws_iam_policy.splunk_execution_role.arn
+  role = aws_iam_role.splunk_execution_role.name
 }
