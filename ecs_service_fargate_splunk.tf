20fdc262f5c2c3708b2e8fd3d17dff99ae7d42fe
Eric Dahl <eric.c.dahl@gmail.com>
2020-07-19 20:23:07 -0700

    spunk: alb/https/cert initial

--- ecs_service_fargate_splunk.tf
+++ ecs_service_fargate_splunk.tf
@@ -60,33 +60,187 @@ resource "aws_security_group_rule" "splunk_ingress_8000_hec" {
 
   cidr_blocks = [var.admin_cidr]
 }
-
-resource "aws_ecs_service" "splunk" {
-
-  name            = "splunk"
-  cluster         = aws_ecs_cluster.cluster.name
-  task_definition = aws_ecs_task_definition.splunk.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  platform_version = "1.4.0"
-
-  network_configuration {
-    security_groups = [
-      aws_security_group.splunk.id,
-      module.vpc.sg_allow_egress,
-      module.vpc.sg_allow_vpc,
-    ]
-
-    subnets          = [module.vpc.subnet_public1]
-    assign_public_ip = true
-  }
-
-  deployment_minimum_healthy_percent = 0
-
-}
+//
+//resource "aws_ecs_service" "splunk" {
+//
+//  name            = "splunk"
+//  cluster         = aws_ecs_cluster.cluster.name
+//  task_definition = aws_ecs_task_definition.splunk.arn
+//  desired_count   = 1
+//  launch_type     = "FARGATE"
+//
+//  platform_version = "1.4.0"
+//
+//  network_configuration {
+//    security_groups = [
+//      aws_security_group.splunk.id,
+//      module.vpc.sg_allow_egress,
+//      module.vpc.sg_allow_vpc,
+//    ]
+//
+//    subnets          = [module.vpc.subnet_public1]
+//    assign_public_ip = true
+//  }
+//
+//  load_balancer {
+//    container_name = "splunk"
+//    container_port = 8088
+//
+//    target_group_arn = aws_lb_target_group.splunk_alb_hec.arn
+//  }
+//
+//  load_balancer {
+//    container_name = "splunk"
+//    container_port = 8000
+//
+//    target_group_arn = aws_lb_target_group.splunk_alb_console.arn
+//  }
+//
+//
+//  deployment_minimum_healthy_percent = 0
+//  health_check_grace_period_seconds = 180
+//
+//
+//  # Service will fail to be created if the ALB isn't there yet
+//  depends_on = [aws_lb.splunk_alb]
+//}
 
 resource "aws_cloudwatch_log_group" "splunk" {
   name = "tf_splunk_fargate_efs"
 }
 
+resource "aws_security_group" "splunk_alb" {
+  vpc_id = module.vpc.vpc_id
+
+  name = "splunk-alb"
+}
+
+resource "aws_security_group_rule" "splunk_alb_ingress_8088_admin" {
+
+  security_group_id = aws_security_group.splunk_alb.id
+
+  type        = "ingress"
+  protocol    = "tcp"
+  from_port   = 8088
+  to_port     = 8088
+  cidr_blocks = [var.admin_cidr]
+}
+
+resource "aws_security_group_rule" "splunk_alb_egress_all" {
+  security_group_id = aws_security_group.splunk_alb.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "splunk_alb_ingress_8088_vpc" {
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 8088
+  to_port           = 8088
+  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id = aws_security_group.splunk_alb.id
+}
+
+resource "aws_lb" "splunk_alb" {
+  name = "splunk-alb"
+
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.splunk_alb.id
+  ]
+
+  subnets = [
+    module.vpc.subnet_public1,
+    module.vpc.subnet_public2,
+    module.vpc.subnet_public3
+  ]
+}
+
+resource "aws_acm_certificate" "splunk" {
+  domain_name = aws_route53_record.splunk_alb.fqdn
+  validation_method = "DNS"
+
+  tags = {
+    Name = aws_route53_record.splunk_alb.fqdn
+  }
+}
+
+resource "aws_route53_record" "acm_validation" {
+  name =  aws_acm_certificate.splunk.domain_validation_options[0].resource_record_name
+  type = aws_acm_certificate.splunk.domain_validation_options[0].resource_record_type
+  zone_id = var.route53_zone_id
+
+  ttl = 15
+
+  records = [
+    aws_acm_certificate.splunk.domain_validation_options[0].resource_record_value
+  ]
+}
+
+//resource "aws_lb_listener" "splunk_console" {
+//  load_balancer_arn = aws_lb.splunk_alb.arn
+//  port = 443
+//  protocol          = "HTTPS"
+//  ssl_policy        = "ELBSecurityPolicy-2016-08"
+//  default_action {
+//    type = "forward"
+//    target_group_arn = aws_lb_target_group.splunk_alb_console.arn
+//  }
+//
+//  certificate_arn = aws_acm_certificate.cert.arn
+//
+//  depends_on = [
+//  aws_route53_record.acm_validation
+//  ]
+//}
+//
+//resource "aws_lb_listener" "splunk_hec" {
+//  load_balancer_arn = aws_lb.splunk_alb.arn
+//  port = 8088
+//  protocol          = "HTTPS"
+//  ssl_policy        = "ELBSecurityPolicy-2016-08"
+//  default_action {
+//    type = "forward"
+//    target_group_arn = aws_lb_target_group.splunk_alb_hec.arn
+//  }
+//
+//  certificate_arn = aws_acm_certificate.cert.arn
+//
+//  depends_on = [
+//    aws_route53_record.acm_validation
+//  ]
+//}
+
+resource "aws_lb_target_group" "splunk_alb_hec" {
+  vpc_id = module.vpc.vpc_id
+  name        = "splunk-tg-hec"
+  port        = 8088
+  protocol    = "HTTP" # TODO: HTTPS?
+  target_type = "ip"
+
+}
+
+resource "aws_lb_target_group" "splunk_alb_console" {
+  vpc_id = module.vpc.vpc_id
+  name        = "splunk-tg-console"
+  port        = 8000
+  protocol    = "HTTP"
+  target_type = "ip"
+}
+
+
+resource "aws_route53_record" "splunk_alb" {
+  zone_id = var.route53_zone_id
+  name    = "splunk"
+  type    = "CNAME"
+
+  ttl = 15
+
+  records = [
+    aws_lb.splunk_alb.dns_name
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,6 @@ variable "public_key" {
 variable "enable_fargate_httpbin_firehose" {
   default = true
 }
+
+variable "route53_zone_id" { // TODO: make toggle-able?
+}
